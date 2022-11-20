edd46bc35fe970ab623d997ad3d56abdfbbc8288
Marcus H <78554363+MarcusH3@users.noreply.github.com>
2022-11-20 10:41:04 +0100

    Frågor

--- src/Questions.java
+++ src/Questions.java
@@ -1,2 +1,11 @@
 public class Questions {
+    String quest;
+    String correct;
+/* Frågeklass med initialiserings objekt att knyta till
+ lista eller array från annan fråge klass för att lagra objekt*/
+    public Questions(String quest,String correct){
+        this.quest = quest;
+        this.correct = correct;
+
+    }
 }
