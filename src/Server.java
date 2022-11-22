2724b6cd9ace4dc0af2cd36b63ffe21e672e3592
Marcus H <78554363+MarcusH3@users.noreply.github.com>
2022-11-22 22:48:11 +0100

    New client /server with question template

--- src/Server.java
+++ src/Server.java
@@ -4,88 +4,77 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server implements Runnable {
-
-    public boolean finish;
-    public ServerSocket server;
-    public ExecutorService treadpool;
-    public ArrayList<ConnectionHandler> connect;
-
-
-    public Server(){
-        connect = new ArrayList<>();
-        finish = false;
-    }
-
-    @Override
-    public void run() {
+public class Server {
+    //TODO: Anropa GUI från Serversida
+    public static void main(String[] args) throws Exception {
+        ServerSocket listener = new ServerSocket(9999);
+        System.out.println("Server för Quizkampen kör");
         try {
-
-            server = new ServerSocket(9999);
-            treadpool = Executors.newCachedThreadPool();
-            while(!finish ) {
-                Socket client = server.accept();
-                ConnectionHandler handler = new ConnectionHandler(client);
-                connect.add(handler);
-                treadpool.execute(handler);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace(System.out);
-
-        }}
-
-    class ConnectionHandler implements Runnable {
-
-        private Socket client;
-        public String playerName;
-
-        private BufferedReader in;
-        private PrintWriter out;
-
-        public ConnectionHandler(Socket client){
-            this.client = client;
-        }
-        @Override
-        public void run() {
-            try {
-                out = new PrintWriter(client.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                out.println("Välkommen till Quizkampen \n" +
-                        "Välj ett namn:");
-                playerName = in.readLine();
-                System.out.println(playerName + " gick med i Quizkamp matchen");
-
-            } catch (Exception e) {
-                shutdown();
-            }
-        }
-
-        public void shutdown () {
-            try{
-                in.close();
-                out.close();
-                if (!client.isClosed()) {
-                    client.close();
+            Quiz.Player waiting = null;
+            Quiz quiz = new Quiz();
+            while (true) {
+                if (waiting == null) {
+                    waiting = quiz.new Player(listener.accept(), " Ett");
+                } else {
+                    Quiz.Player playerTwo = quiz.new Player(listener.accept(), " Två");
+                    waiting.setOpponent(playerTwo);
+                    playerTwo.setOpponent(waiting);
+                    waiting.start();
+                    playerTwo.start();
                 }
-            } catch (IOException e){
-                e.printStackTrace(System.out);
             }
-
+        } finally {
+            listener.close();
         }
     }
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.run();
+}
+class Quiz {
+    Quiz quiz;
+
+    public Quiz() {
+        quiz = new Quiz();
+    }
+
+    class Player extends Thread {
+        Player opponent;
+        Socket socket;
+        BufferedReader in;
+        PrintWriter out;
+        String name;
+        String answer;
+        boolean answered;
+
+        public Player(Socket socket, String name) {
+            this.socket = socket;
+            this.name = name;
+            try {
+                in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+
+            }
+        }
+
+        public void setOpponent(Player opponent) {
+            this.opponent = opponent;
+        }
     }
 }
 
+        ///TODO: Hitta ett sätt att hantera frågorna genom ex bool, string anserw om anserw = true eller anserw =
+        // TODO: correct answer
+        /*public void run() {
+            try {
 
-
-
-
-
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e){}
+            }
+        }
+    }*/
