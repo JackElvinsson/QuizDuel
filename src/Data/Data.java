ad46420b8f32e6687a13183ed1cc7b001421485c
Marcus H <78554363+MarcusH3@users.noreply.github.com>
2022-11-26 14:33:22 +0100

    Data package & class added

---
+++ src/Data/Data.java
@@ -0,0 +1,12 @@
+package Data;
+
+import java.io.Serializable;
+
+public class Data implements Serializable {
+
+    public int player;
+    public String playerName;
+    public String opponentName;
+    public List<> playerOneScore;
+    public List<> opponentScore;
+}
